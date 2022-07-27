4527835cc772ad5706577127fe02c66a9123e8e1
BhanukaAGH <it20009472@my.sliit.lk>
2022-07-27 19:19:45 +0530

    create authentication and movie modal view

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import useAuth from '../hooks/useAuth'
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false)
+  const { logout } = useAuth()
 
   useEffect(() => {
     const handleScroll = () => {
@@ -44,13 +46,15 @@ const Header = () => {
         <SearchIcon className='hidden sm:inline h-6 w-6' />
         <p className='hidden lg:inline'>Kids</p>
         <BellIcon className='h-6 w-6' />
-        <Link href={'/account'}>
+        {/* <Link href={'/account'}> */}
+        <div onClick={logout}>
           <img
             src='https://rb.gy/g1pwyx'
             alt=''
             className='cursor-pointer rounded'
           />
-        </Link>
+        </div>
+        {/* </Link> */}
       </div>
     </header>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head'
+import { useRecoilValue } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
 import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 
@@ -26,10 +30,15 @@ const Home = ({
   romanceMovies,
   documentaries,
 }: Props) => {
+  const { loading } = useAuth()
+  const showModal = useRecoilValue(modalState)
+
+  if (loading) return null
+
   return (
     <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
       <Head>
-        <title>Create Next App</title>
+        <title>Home - Netflix</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
@@ -49,7 +58,7 @@ const Home = ({
           <Row title='Documentaries' movies={documentaries} />
         </section>
       </main>
-      {/* Modal */}
+      {showModal && <Modal />}
     </div>
   )
 }
